887bcfec5947408c077ac743d30db7c34add8310
islomovic <92381015+islomovic@users.noreply.github.com>
2021-12-30 15:35:37 +0500

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,43 @@
+
+// let username = "admin"
+// let a = prompt("login");
+// if (a === username){
+//   console.log(a);
+// }
+// else {
+//   alert("bomadi");
+// }
+
+// let password = "12345"
+// let b = prompt("password")
+// if (b === password){
+//   console.log(b);
+//   alert(" salom ");
+// }
+
+
+
+// for (aim = 0; aim <= 100; aim++){
+//   if (aim % 2 == 0){
+//     console.log(aim);;
+//   }
+// }
+
+
+// for( aim = 0; aim <= 100; aim++)
+//         if(aim%3 == 0)
+//         console.log(aim);
+
+
+  // for( aim = 0; aim <= 100; aim++)
+  //       if(aim%5 == 0)
+  //       console.log(aim);
+
+  
+let negr;
+
+do {
+  negr = prompt("enter a number greater than 100?", 100);
+}
+while (negr < 100 && negr);
+
